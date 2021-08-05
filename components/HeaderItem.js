508cd80e6ec0fae1a99a74119556646da6925ea5
OkYNaS <mrinsanebro@gmail.com>
2021-08-05 16:48:35 +0200

    Finishing add routes
    routes include home, trending, verified, collections, search, account

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,10 +1,16 @@
-function HeaderItem ({Icon, title}) {
+import Link from "next/link"
+
+function HeaderItem ({Icon, title, link}) {
+
+  // console.log(link)
   return (
-    <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white content-center mt-8">
-      <Icon className="h-8 mb-1 grouphover:animate-bounce"/>
-      <p className="opacity-0 group-hover:opacity-100
-      tracking-widest">{title}</p>
-    </div>
+    <Link href={link}>
+      <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white content-center mt-8">
+        <Icon className="h-8 mb-1 grouphover:animate-bounce"/>
+        <p className="opacity-0 group-hover:opacity-100
+        tracking-widest">{title}</p>
+      </div>
+    </Link>
   )
 }
 
